ed51c3e8b8f284d4ab7ebd88f5886ec8e744362a
Artvell3000 <86819826+Artvell3000@users.noreply.github.com>
2025-06-04 16:12:21 +0600

    added localization

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:whoami_app/app/theme/theme.dart';
 import 'package:whoami_app/core/routing/router.dart';
+import 'package:whoami_app/gen_locales/app_localizations.dart';
 
 late final AppRouter _appRouter;
 
@@ -11,6 +13,13 @@ class WhoAmIApp extends StatelessWidget {
   Widget build(BuildContext context) {
     _appRouter = AppRouter();
     return MaterialApp.router(
+      locale: const Locale('en'),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: whoAmIAppTheme,
       routerConfig: _appRouter.config(),
     );
